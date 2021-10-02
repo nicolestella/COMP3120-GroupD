daec1758187ecede0a85d9108a3536de4b628f9c
PeterWuMQ <80020886+PeterWuMQ@users.noreply.github.com>
2021-10-02 14:49:25 +1000

    Changed /api/info

--- ass-2-group-d/server/controller/api.js
+++ ass-2-group-d/server/controller/api.js
@@ -10,13 +10,15 @@ apiRouter.get('/api/movies', (request, response) => {})
 
 apiRouter.get('/api/movies/:movieid', (request, response) => {})
 
-apiRouter.get('/api/info/:movieid/:userid', (request, response) => {})
+apiRouter.get('/api/info/:userid', (request, response) => {})
 
-apiRouter.post('/api/info/:movieid/:userid', (request, response) => {})
+apiRouter.get('/api/info/:userid:/movieid', (request, response) => {})
 
-apiRouter.put('/api/info/:movieid/:userid', (request, response) => {})
+apiRouter.post('/api/info/:userid:/movieid', (request, response) => {})
 
-apiRouter.delete('/api/info/:movieid/:userid', (request, response) => {})
+apiRouter.put('/api/info/:userid/:movieid', (request, response) => {})
+
+apiRouter.delete('/api/info/:userid/:movieid', (request, response) => {})
 
 
 module.exports = apiRouter
