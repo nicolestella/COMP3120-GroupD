dd5b3b2da9d7f405f5d4ed18694035267753e8e4
MendoMq <69186120+MendoMq@users.noreply.github.com>
2021-10-08 15:22:21 +1100

    Movies services API GET

--- ass-2-group-d/src/services/movies.js
+++ ass-2-group-d/src/services/movies.js
@@ -1,7 +1,30 @@
 import axios from 'axios'
+const baseURL = 'https://api.themoviedb.org/3/movie'
 
 const movies = () => {
-
+	return axios
+		.get(
+			baseURL + '/top_rated?api_key=d376228a124d9077ef3238a86c282792&language=en-US&page=1%27',
+		)
+		.then(responce => responce.data.results)
 }
 
-export default movies
+const movie = (movieid) => {
+	return axios
+		.get(
+			baseURL + '/' + movieid + '?api_key=d376228a124d9077ef3238a86c282792&language=en-US',
+		)
+		.then(responce => responce.data.results)
+}
+
+export default { movies, movie }
+
+/*
+axios
+	.get(
+		'https://api.themoviedb.org/3/movie/top_rated?api_key=d376228a124d9077ef3238a86c282792&language=en-US&page=1%27',
+	)
+	.then(function (responce) {
+		console.log(responce.data.results)
+	})
+*/
