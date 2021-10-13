315e48c22178d989ba43d42b13e431cc1ea1f382
Juice713 <87959733+Juice713@users.noreply.github.com>
2021-10-13 12:03:40 +1100

    added server/server.js and server/app.js

---
+++ ass-2-group-d/server/app.js
@@ -0,0 +1,12 @@
+require('dotenv').config()
+
+const express = require('express')
+const cors = require('cors')
+const apiRouter = require('./controller/api')
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(apiRouter)
+
+module.exports = app
