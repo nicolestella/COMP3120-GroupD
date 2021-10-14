f6716593b248152730f74cd536243d242bc17231
nicolestella <nicolestella@users.noreply.github.com>
2021-10-14 19:45:49 +1100

    added theme

---
+++ ass-2-group-d/src/config/theme.js
@@ -0,0 +1,23 @@
+import { createTheme } from '@mui/material/styles'
+
+const theme = createTheme({
+	palette: {
+		background: '#EDEDED',
+	},
+	typography: {
+		fontFamily: [
+			'-apple-system',
+			'BlinkMacSystemFont',
+			'"Segoe UI"',
+			'Roboto',
+			'"Helvetica Neue"',
+			'Arial',
+			'sans-serif',
+			'"Apple Color Emoji"',
+			'"Segoe UI Emoji"',
+			'"Segoe UI Symbol"',
+		].join(','),
+	},
+})
+
+export default theme
