4179f36948470d8f3dc4d1dfc75a93c8d9121a4f
nicolestella <nicolestella@users.noreply.github.com>
2021-10-22 00:14:40 +1100

    moved movie card to be its own component

--- ass-2-group-d/src/pages/Home.js
+++ ass-2-group-d/src/pages/Home.js
@@ -1,19 +1,11 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-// import styling
-import theme from '../config/theme'
 // import material ui components
-import Card from '@mui/material/Card'
-import CardActionArea from '@mui/material/CardActionArea'
-import Image from 'material-ui-image'
-import Typography from '@mui/material/Typography'
 import Grid from '@mui/material/Grid'
-import Button from '@mui/material/Button'
-import Stack from '@mui/material/Stack'
-// import icons
-import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline'
 // import custom components
+import theme from '../config/theme'
 import Layout from '../components/Layout'
+import MovieCard from '../components/MovieCard'
 import Movies from '../services/movies'
 
 const Home = ({ imgURL }) => {
@@ -24,8 +16,7 @@ const Home = ({ imgURL }) => {
 	}, [])
 
 	return (
-		<Layout>
-			{console.log(movies)}
+		<Layout homeDisabled>
 			<Grid container spacing={4} sx={{ padding: theme.spacing(4) }}>
 				{movies.map((movie) => (
 					<Grid
@@ -33,48 +24,7 @@ const Home = ({ imgURL }) => {
 						item
 						xs={6} sm={4} md={3} lg={2}
 					>
-						<Stack
-							sx={{ width: '100%', height: '100%' }}
-							alignItems="center"
-						>
-							{/* Movie poster */}
-							<Card
-								sx={{
-									width: '100%',
-									aspectRatio: '0.66667',
-								}}
-							>
-								<CardActionArea
-									sx={{ width: '100%', height: '100%' }}
-								>
-									<Image
-										src={`${imgURL}/${movie.poster_path}`}
-										style={{ position: 'initial' }}
-										// cover
-									/>
-								</CardActionArea>
-							</Card>
-
-							{/* Add to watchlist button */}
-							<Button>
-								<AddCircleOutlineIcon />
-								<Typography
-									variant="body3"
-									sx={{ marginLeft: theme.spacing(2) }}
-								>
-								Add to watchlist
-								</Typography>
-							</Button>
-
-							{/* Movie title */}
-							<Typography
-								sx={{ fontWeight: 'bold' }}
-								variant="h6"
-								align="center"
-							>
-								{movie.title}
-							</Typography>
-						</Stack>
+						<MovieCard img={`${imgURL}/${movie.poster_path}`} title={movie.title} />
 					</Grid>
 				))}
 			</Grid>
